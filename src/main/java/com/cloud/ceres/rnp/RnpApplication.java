8a7c210a2f6d40dcdad8d763bf20153c9cf08cb2
humingming <1158139789@qq.com>
2020-10-09 17:11:41 +0800

    胡明明

--- src/main/java/com/cloud/ceres/rnp/RnpApplication.java
+++ src/main/java/com/cloud/ceres/rnp/RnpApplication.java
@@ -13,12 +13,15 @@ public class RnpApplication {
 
 
     public static void main(String[] args) throws UnknownHostException {
+        System.out.println("启动服务");
         ConfigurableApplicationContext application =  SpringApplication.run(RnpApplication.class,args);
+        System.out.println("服务启动完成");
         Environment env = application.getEnvironment();
         String ip = InetAddress.getLocalHost().getHostAddress();
         String port = env.getProperty("server.port");
         String path = env.getProperty("server.servlet.context-path");
-        System.out.println("\n----------------------------------------------------------\n\t" +
+        System.out.println("\n----------------------------------------------------------\n\t"+
+                path +
                 "Application is running! Access URLs:\n\t" +
                 "Local: \t\thttp://localhost:" + port  + "/\n\t" +
                 "External: \thttp://" + ip + ":" + port  + "/\n\t" +
